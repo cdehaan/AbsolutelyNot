b7a3e832ac9f0d996235dfedfa7f4a55f4423a3e
cdehaan <cdehaan@gmail.com>
2023-08-08 11:57:13 +0900

    const dispatch = useDispatch() was in the wrong place

--- SocialMediaLogin.tsx
+++ SocialMediaLogin.tsx
@@ -15,8 +15,6 @@ import { SigninStatus, setGoogleUser, setSignedIn } from "./store/slices/googleA
 import { removePlayer, setLastAction, setName, setPicture } from "./store/slices/player";
 import { RootState } from "./store/store";
 
-const dispatch = useDispatch()
-
 // https://github.com/react-native-google-signin/google-signin#3-userinfo
 type GoogleUserData = {
     idToken: string | null,
@@ -57,7 +55,7 @@ async function UrlToBase64(url: string): Promise<string> {
   
 function SocialMediaLogin() {
     const googleUser = useSelector((state: RootState) => state.googleUser)
-    //const { isSignedIn, name, email, profilePictureURL, internalID } = useSelector((state: RootState) => state.user)
+    const dispatch = useDispatch()
 
     useEffect(() => {
         GoogleSignin.configure({
